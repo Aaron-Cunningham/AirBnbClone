42fca830325589eda51af615b66e618d79ebd115
Aaron-Cunningham <aaroncunningham222@gmail.com>
2024-01-20 23:35:13 +0000

    - Updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+    domains: [
+      'links.papareact.com',
+      'i.pinimg.com',
+      'i.imgur.com',
+      'wallpapercave.com'
+    ]
+  }
 };
 
 export default nextConfig;
