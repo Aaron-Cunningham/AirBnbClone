88446bba0b309166b3739680a295249443b7ce4b
Aaron-Cunningham <aaroncunningham222@gmail.com>
2024-01-21 00:52:48 +0000

    - Updated tailwind for small card

--- src/components/SmallCard.js
+++ src/components/SmallCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 function SmallCard({img, location, distance}) {
   return (
-    <div className='flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 transition transform duration-200 hover:scale-105 ease-out '>
+    <div className='flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 transition transform duration-200 hover:scale-105 ease-out'>
         <div className='relative h-16 w-16'>
             <Image 
             className='rounded-lg'
