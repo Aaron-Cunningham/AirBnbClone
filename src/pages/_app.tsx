f66dffa6be00ab4f3450bee25c00187665895a43
Aaron-Cunningham <aaroncunningham222@gmail.com>
2024-01-21 02:26:00 +0000

    - Added Progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
+
+
+// Progress bar
+const progress = new ProgressBar({
+  size:4,
+  color: "#FE595E",
+  className: "z-50",
+  delay: 50
+  
+})
+
+
+// Progress bar states
+Router.events.on('routeChangeStart', progress.start)
+Router.events.on('routeChangeComplete', progress.finish)
+Router.events.on("routeChangeError", progress.finish)
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
