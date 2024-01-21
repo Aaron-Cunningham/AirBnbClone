01814c1d4fcce8e5d7e4888bc0cc56939bb606cc
Aaron-Cunningham <aaroncunningham222@gmail.com>
2024-01-21 02:16:54 +0000

    - Added search page

---
+++ src/pages/search.js
@@ -0,0 +1,77 @@
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+import { useRouter } from "next/router"
+import {format} from "date-fns"
+import InfoCard from "../components/InfoCard";
+
+
+function Search({searchResults}) {
+
+    const router = useRouter();
+
+    // Data pulled in from header
+    const {location, startDate, endDate, guests} = router.query;
+
+    const formatteStartDate = format(new Date(startDate), "dd MMMM yy")
+    const formattedEndDate = format(new Date(endDate), "dd MMMM yy")
+    const range = `${formatteStartDate} - ${formattedEndDate}`
+
+
+  return (
+    <div className="">
+        <Header
+        placeholder={`${location} | ${range} | ${guests} guests`} />
+
+        <main className="flex">
+            <section className="flex-grow pt-14 px-6">
+                <p className="text-xs">300+ Stays - {range} - for {guests}  Guests</p>
+                <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in {location}</h1>
+                <div className="hidden md:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+                    <p className="button">Cancellation Flexibility</p>
+                    <p className="button">Type of place</p>
+                    <p className="button">Price</p>
+                    <p className="button">Rooms and beds</p>
+                    <p className="button">More Filters</p>
+                </div>
+                <div className="flex flex-col">
+
+                    {searchResults?.map(items => (
+                        <InfoCard
+                        key={items.img}
+                        img={items.img}
+                        location={items.location}
+                        title={items.title}
+                        description={items.description}
+                        star={items.star}
+                        price={items.price}
+                        total={items.total}
+                        />
+
+                    ))}
+
+                </div>
+
+            </section>
+
+        </main>
+        <Footer/>
+        
+    </div>
+  )
+}
+
+export default Search
+
+
+// Server side rendering
+
+export async function getServerSideProps(){
+    const searchResults = await fetch("https://www.jsonkeeper.com/b/5NPS").then(res => res.json())
+    return{
+        props:{
+            searchResults: searchResults
+        }
+    }
+}
+
+
